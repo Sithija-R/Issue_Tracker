60c3cb3a393dc1a25c9d39c6c29f7de918be0cb7
Sithija R <Sithijarathukorale@gmail.com>
2025-07-25 19:05:57 +0530

    backend basic configurations

---
+++ Backend/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const connectDB = require("./config/dbconfig");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.get("/", (req,res) =>{
+    res.send("Hello from backend");
+})
+
+connectDB().then(()=>{
+    app.listen(PORT,()=>{
+        console.log("Server is running on port: ",PORT);
+    })
+})
